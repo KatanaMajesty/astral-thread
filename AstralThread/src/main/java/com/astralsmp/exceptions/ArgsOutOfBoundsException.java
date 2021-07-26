f3f9da46f7bf2af861154c97ea1ff631e9098523
KatanaMajesty <sasha.panasenk@gmail.com>
2021-07-26 17:46:01 +0300

    @Deprecated

--- AstralThread/src/main/java/com/astralsmp/exceptions/ArgsOutOfBoundsException.java
+++ AstralThread/src/main/java/com/astralsmp/exceptions/ArgsOutOfBoundsException.java
@@ -1,5 +1,9 @@
 package com.astralsmp.exceptions;
 
+/**
+ * Метод использовался для проверки на правильность работы счётчика аргументов и кастомных исключений
+ */
+@Deprecated
 public class ArgsOutOfBoundsException extends AstralException {
     public ArgsOutOfBoundsException(int argCount) {
         super("Неверное количество аргументов: " + argCount);
